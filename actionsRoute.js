465f9d8a90723ee48ff2ed413dec7fd05d341cfe
Daniel <danielstarling4@gmail.com>
2019-03-22 08:45:35 -0700

    actions post working

--- actionsRoute.js
+++ actionsRoute.js
@@ -5,23 +5,36 @@ const db = require("./data/helpers/actionModel");
 
 router.get("/", async (req, res) => {
   try {
-    const actions = await req.body;
-    db.get().then(actions => {
+    const actions = req.body;
+    await db.get().then(actions => {
       res.status(200).json({ actions: actions });
     });
   } catch (err) {
     res.status(500).json({ error: err });
   }
 });
+
 router.get("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const actions = await req.body;
-    db.get(id).then(actions => {
+    const actions = req.body;
+    await db.get(id).then(actions => {
       res.status(200).json({ actions: actions });
     });
   } catch (err) {
     res.status(500).json({ error: err });
   }
 });
+
+router.post("/", async (req, res) => {
+  try {
+    const newAction = req.body;
+    await db.insert(newAction).then(response => {
+      res.status(201).json(response);
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
 module.exports = router;
